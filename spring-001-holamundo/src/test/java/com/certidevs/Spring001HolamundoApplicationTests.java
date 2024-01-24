a9659172ed32b8f815b328ab7d0e473c035d72ea
AngelSigueros <pickassblog@gmail.com>
2024-01-24 11:27:01 +0100

    Spring first proyect

---
+++ spring-001-holamundo/src/test/java/com/certidevs/Spring001HolamundoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certidevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring001HolamundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
