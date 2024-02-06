49a01742b81373e679e9b110accad8b4f67ae125
AngelSigueros <pickassblog@gmail.com>
2024-02-06 13:25:47 +0100

    CrossOrigin

--- sm-app/src/main/java/com/sas/sm_app/controller/HomeController.java
+++ sm-app/src/main/java/com/sas/sm_app/controller/HomeController.java
@@ -1,9 +1,9 @@
 package com.sas.sm_app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
+@CrossOrigin(origins = "http://localhost:4200")
 @Controller
 public class HomeController {
 
